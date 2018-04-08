b8deddda3b7873ea669cd936d10e84605cefb06d
Mahmoud khalil <khalil2535@yahoo.com>
2018-04-08 17:16:58 +0300

    Update DemoArrays.java
    * using Random class
    * reducing memory usage

--- Arrays/DemoArrays.java
+++ Arrays/DemoArrays.java
@@ -4,24 +4,30 @@ public class DemoArrays {
 
     public static void main(String[] args) {
 
-        int[] numbers = {5, 2, 6, 2, 3, 7, 34, 25, 25, 6, 3, 2};
+        int[] numbers = new int[10];
+
+        java.util.Random ran = new java.util.Random(); // Random class to generate numbers
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = ran.nextInt(100); // generating numbers between (0 -> 100)
+        }
 
         for (int n : numbers) {
-            System.out.print("" + n + " ");
+            System.out.print("" + n + ' ');
         }
         System.out.println();
         insertionSort2(numbers);
 
         for (int n : numbers) {
-            System.out.print("" + n + " ");
+            System.out.print("" + n + ' ');
         }
         System.out.println();
 
     }
 
     public static void insertionSort(int[] numbers) {
-        int length = numbers.length;
-        for (int i = 1; i < length; i++) {
+        //int length = numbers.length;
+        for (int i = 1; i < numbers.length; i++) {
 
             for (int j = i; j > 0; j--) {
                 if (numbers[j - 1] > numbers[j]) {
@@ -35,8 +41,8 @@ public class DemoArrays {
     }
 
     public static void insertionSort2(int[] numbers) {
-        int length = numbers.length;
-        for (int i = 1; i < length; i++) {
+        //int length = numbers.length;
+        for (int i = 1; i < numbers.length; i++) {
 
             int cur = numbers[i];
             int j = i;
